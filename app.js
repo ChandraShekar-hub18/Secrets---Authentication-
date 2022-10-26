fe908e2195df7ab863b843da3f0cc5d21819aa63
Chandra Shekar <chandrashekar.mekala2001@gmail.com>
2022-10-26 09:21:52 +0530

    level:2 Encryption password

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
 
 const app = express();
 
@@ -15,10 +16,13 @@ app.use(bodyParser.urlencoded({
 
 mongoose.connect("mongodb+srv://chandrashekar:chandu123@cluster0.ohokhxh.mongodb.net/userDB")
 
-const userSchema = {
+const userSchema = mongoose.Schema({
   email: String,
   password: String
-}
+})
+
+const secret = "Thisourlittlesecret.";
+userSchema.plugin(encrypt, { secret: secret, encryptedFields: ["password"]})
 
 const User = new mongoose.model("User",userSchema);
 
@@ -29,6 +33,23 @@ app.get("/", ( req, res)=>{
 
 app.get("/login", ( req, res)=>{
   res.render("login");
+})
+
+app.post("/login", ( req, res)=>{
+  const email = req.body.username;
+  const password = req.body.password;
+console.log(email, password);
+  User.findOne({email: email}, (err, foundUser)=>{
+    if(foundUser){
+      if(foundUser.password === password){
+        res.render("secrets");
+      } else{
+        res.send("Wrong password");
+      }
+    }else{
+      res.send("err");
+    }
+  })
 })
 
 app.get("/register", ( req, res)=>{
@@ -49,6 +70,9 @@ app.post("/register", ( req, res)=>{
   })
 })
 
+app.get("/logout", (req, res)=>{
+  res.redirect("/");
+})
 
 
 app.listen(3000,()=>{
